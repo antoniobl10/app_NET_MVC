cda01c148d92f4d9645dc84ed5d084811c712e68
antoniobl10 <antoniblanc.aba@gmail.com>
2023-12-16 12:48:28 +0100

    views + CRUD controller + toastr

---
+++ AntonioWeb/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using AntonioWeb.Data;
+using AntonioWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AntonioWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "Display Order can not be equal to Name");
+            //}
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+            //Category? categoryFromDB1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDB2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AntonioWeb/Migrations/20231215171649_SeedCategoryTable2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntonioWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DisplayOrder",
+                value: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DisplayOrder",
+                value: 1);
+        }
+    }
+}
